fc5a58bdf4eef177266cd072107d39a63d09b2f3
Dhita Irma <dhita.irma@gmail.com>
2020-10-27 23:12:53 +0800

    Create register view

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
     return render(request, "booking/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "booking/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try: 
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "booking/regiseter.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return redirect(reverse("index"))
+    return render(request, "booking/register.html")
+
+
+def login_view(request):
+    return render(request, "booking/login.html")
