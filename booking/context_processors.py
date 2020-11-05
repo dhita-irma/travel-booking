84e9d6c3e8c1fc62237bc45e8a4056b015941af8
Dhita Irma <dhita.irma@gmail.com>
2020-11-05 11:35:51 +0800

    Fix context_processors.py

--- booking/context_processors.py
+++ booking/context_processors.py
@@ -2,5 +2,8 @@ from booking.models import Order
 
 
 def layout(request):
-    order, created = Order.objects.get_or_create(user=request.user, complete=False)
+    if request.user.is_authenticated:
+        order, created = Order.objects.get_or_create(user=request.user, complete=False)
+    else:
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
     return {'order': order}
