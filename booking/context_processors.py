4c9abcd5ce00199495c20335a047ec125efafd4e
Dhita Irma <dhita.irma@gmail.com>
2020-11-14 17:00:46 +0800

    Update context_processors.py

--- booking/context_processors.py
+++ booking/context_processors.py
@@ -1,9 +1,13 @@
-from booking.models import Order
+from booking.models import Destination, Order
 
 
 def layout(request):
+    destinations = Destination.objects.all()
     if request.user.is_authenticated:
         order, created = Order.objects.get_or_create(user=request.user, complete=False)
     else:
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-    return {'order': order}
+    return {
+        'order': order,
+        'destinations': destinations,
+        }
