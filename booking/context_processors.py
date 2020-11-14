a5fe1d5e9f6935d191779a117499472d33695128
Dhita Irma <dhita.irma@gmail.com>
2020-11-14 17:25:02 +0800

    Update navbar items

--- booking/context_processors.py
+++ booking/context_processors.py
@@ -5,6 +5,7 @@ def layout(request):
     destinations = Destination.objects.all()
     if request.user.is_authenticated:
         order, created = Order.objects.get_or_create(user=request.user, complete=False)
+        user = request.user
     else:
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     return {
