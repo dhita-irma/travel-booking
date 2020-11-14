11ad686463f19a063f84d355cedb6abe6ffcbd80
Dhita Irma <dhita.irma@gmail.com>
2020-11-14 16:33:33 +0800

    Style filtered destinations cards

--- booking/static/booking/main.js
+++ booking/static/booking/main.js
@@ -27,19 +27,21 @@ function filterDestination(destination) {
             console.log(listings[i]);
   
             const colDiv = document.createElement('div');
-            colDiv.className = `col-lg-4 mb-4 listing ${listings[i].location} ${listings[i].category}`;
+            colDiv.className = `col-lg-4 listing card-padding ${listings[i].location} ${listings[i].category}`;
   
             colDiv.innerHTML = `
-              <div class="card h-100">
-                <img class="card-img-top" src="${listings[i].image_url}" alt="">
-                <div class="card-body p-2">
-                    <h6 class="card-title">${listings[i].title}</h6>
-                    <h6 class="card-subtitle mb-2 text-muted">${listings[i].location}</h6>
-                    <p class="card-text card-desc"></p>
-                </div>
-                <div class="card-footer text-right p-2">
-                    Rp ${listings[i].price}
-                </div>
+              <div class="card h-100 listing-card">
+                  <img class="card-img-top" src="${listings[i].image_url}" alt="">
+                  <div class="card-body p-2">
+                      <a href="{% url 'catalog_item' listing.id %}" class="stretched-link">
+                          <h6 class="card-title">${listings[i].title}</h6>
+                      </a>
+                      <p class="card-subtitle"> <i class="fas fa-map-marker-alt"></i>  ${listings[i].location}</p>
+                      <p class="card-text card-desc"></p>
+                  </div>
+                  <div class="card-footer p-2">
+                      US$ ${listings[i].price}<span>/pax</span>
+                  </div>
               </div>
             `
             filteredCatalog.appendChild(colDiv);
